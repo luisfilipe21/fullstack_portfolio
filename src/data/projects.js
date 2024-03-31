b0a0eb79e30265cfbfe5b19148866a0f3ee39102
Luis Filipe Guimarães Costa <luisfilipegc@hotmail.com>
2024-03-31 18:33:08 -0300

    feat: database updated

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,7 +5,7 @@ export const projects = [
         description: "This project was carried out in a group with the main objective of improving our knowledge of Git Flow. Developed in React and styled in SCSS and with a clean and responsive design, this site simulates a film review page. Allowing users to register themselves and log in to rate the films listed once watched.",
         site: "https://kenzie-movie-five.vercel.app/",
         img: "https://i.imgur.com/mtpHIM9.png",
-        technologies: ["React", "Javascript","SCSS", "HTML"],
+        technologies: ["React", "Javascript", "SCSS", "HTML"],
         git: "https://github.com/"
     },
     {
@@ -38,7 +38,7 @@ export const projects = [
     {
         id: 4,
         name: "Social Media",
-        description: "",
+        description: "This is social midia app, where you can create posts, like and open your friend's full post.  There is also the option to start to follow new people inside the page. And since this project is in Javascript vanilla, there is an abundunce of async functions.",
         site: "https://rede-social-bice.vercel.app/",
         img: "https://i.imgur.com/bHM5DKS.png",
         technologies: ["Javascript", "CSS", "HTML"],
@@ -47,7 +47,7 @@ export const projects = [
     {
         id: 5,
         name: "Nu Kenzie",
-        description: "",
+        description: "This is a simulation of a finance control app, where we can control the inflow and outflow of money, returning the final balance. We may also delete one card once it is no longer needed or has been wrongly added.",
         site: "https://kenzie-academy-brasil-developers-react-entrega-s1-luisfilipe21.vercel.app/",
         img: "https://i.imgur.com/DeOvJ7t.png",
         technologies: ["Javascript", "SCSS", "HTML"],
@@ -56,7 +56,7 @@ export const projects = [
     {
         id: 6,
         name: "Pet Info",
-        description: "",
+        description: "In this project I creation of a social network for pets, where you can create posts, like and comment on the posts of your pet friends. There is also a schema for creating and validating logins via Zod, and just a touch to make a better UX for project was usage of the Toastify library. And since this project is in Javascript vanilla, there is an abundunce of async functions. The Pet Api was provided by Kenzie Academy Brasil.",
         site: "https://kenzie-academy-brasil-developers-pet-info-template-luisfilipe21.vercel.app/",
         img: "https://i.imgur.com/OJ87HP7.png",
         technologies: ["Javascript", "CSS", "HTML"],
@@ -65,7 +65,7 @@ export const projects = [
     {
         id: 7,
         name: "Rifa Alexa",
-        description: "",
+        description: "This was an ordered project by a client who wanted to raffle an Echo Dot AKA Alexa. A list of 50 names was created to be chosen for the random draw. When the name was sold, it would be grayed out and could no longer be picked. By his choice, the random draw of the names was not to be included within this application.",
         site: "https://rifa-echodot.vercel.app/",
         img: "https://i.imgur.com/5rsaskK.png",
         technologies: ["Javascript", "CSS", "HTML"],
@@ -74,7 +74,7 @@ export const projects = [
     {
         id: 8,
         name: "Cars API",
-        description: "",
+        description: "This was a project commissioned by a client who wanted to raffle an Echo Dot AKA Alexa. A list of 50 names was created to be chosen for the random draw. When the name was sold, it would be grayed out and could no longer be chosen. By his choice, the random draw of names within this application was not ugly.",
         site: "",
         img: "https://i.imgur.com/CDZETX1.png",
         technologies: ["Typescript", "Jest", "Prisma"],
@@ -83,16 +83,16 @@ export const projects = [
     {
         id: 9,
         name: "Tasks API",
-        description: "",
+        description: "This project was to practice our logic in unit and integration testing of the API we built. Testing all possible routes and errors that each one could return.",
         site: "",
         img: "https://i.imgur.com/vNUYcwe.png",
-        technologies: ["Typescript"],
+        technologies: ["Typescript", "Jest"],
         git: "https://github.com/Kenzie-Academy-Brasil-Developers/Kenzie-Academy-Brasil-Developers-m5-template-entrega1_luisfilipe21"
     },
     {
         id: 10,
         name: "Landing page - Login",
-        description: "",
+        description: "A simple landing page with a login structure and a button. This was mainly a CSS challenge.",
         site: "https://login-landing-page-three.vercel.app/",
         img: "https://i.imgur.com/oP4Cm0y.png",
         technologies: ["Javascript", "HTML", "CSS"],
@@ -101,7 +101,7 @@ export const projects = [
     {
         id: 11,
         name: "Kenzie Hub",
-        description: "fffffffffffffffff",
+        description: "This is an application that creates and validates logins with the help of Zod. Once inside the home page, it is possible to create cards with information about the user. Made with vanilla CSS Javascript and the Toastify library",
         site: "https://kenzie-hub-hazel.vercel.app/",
         img: "https://i.imgur.com/hWHnMJS.png",
         technologies: ["Javascript", "HTML", "SCSS"],
