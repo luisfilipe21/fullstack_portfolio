7a69076e4b24ad3dcb332daf84607b5d1c793954
Luis Filipe Guimarães Costa <luisfilipegc@hotmail.com>
2024-12-26 12:28:43 -0300

    fix: ajustes no update dos svgs

--- src/components/Techs/data/techList.js
+++ src/components/Techs/data/techList.js
@@ -1,82 +1,82 @@
 export const techList = [
     {
         id: 1,
-        img: "src/public/assets/html.svg",
+        img: "public/html.svg",
         name: "HTML",
     },
     {
         id: 2,
-        img: "src/public/assets/css.svg",
+        img: "public/css.svg",
         name: "CSS",
     },
     {
         id: 3,
-        img: "src/public/assets/js.svg",
+        img: "public/js.svg",
         name: "Javascript",
     },
     {
         id: 4,
-        img: "src/public/assets/typescript.svg",
+        img: "public/typescript.svg",
         name: "Typescript",
     },
     {
         id: 5,
-        img: "src/public/assets/react.svg",
+        img: "public/react.svg",
         name: "React",
     },
     {
         id: 6,
-        img: "src/public/assets/nextjs.svg",
+        img: "public/nextjs.svg",
         name: "Next.Js",
     },
     {
         id: 7,
-        img: "src/public/assets/sass.svg",
+        img: "public/sass.svg",
         name: "SASS",
     },
     {
         id: 8,
-        img: "src/public/assets/tailwind.svg",
+        img: "public/tailwind.svg",
         name: "Tailwind",
     },
     {
         id: 9,
-        img: "src/public/assets/git.svg",
+        img: "public/git.svg",
         name: "Git",
     },
     {
         id: 10,
-        img: "src/public/assets/github.svg",
+        img: "public/github.svg",
         name: "GitHub",
     },
     {
         id: 11,
-        img: "src/public/assets/nodejs.svg",
+        img: "public/nodejs.svg",
         name: "NodeJs",
     },
     {
         id: 12,
-        img: "src/public/assets/nestjs.svg",
+        img: "public/nestjs.svg",
         name: "Nest",
     },
     {
         id: 13,
-        img: "src/public/assets/postgresql.svg",
+        img: "public/postgresql.svg",
         name: "PostgreSQL",
     },
     {
         id: 14,
-        img: "src/public/assets/jest.svg",
+        img: "public/jest.svg",
         name: "Jest",
     },
     {
         id: 15,
-        img: "src/public/assets/prisma.svg",
+        img: "public/prisma.svg",
         name: "Prisma ORM",
     },
     {
         id: 16,
-        img: "src/public/assets/express.svg",
+        img: "public/express.svg",
         name: "Express",
     },
 
